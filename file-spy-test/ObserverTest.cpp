2619f3b6b65df10b140ffea8a994ce49f36282d8
Eivydas Senkus <esenkus@digital.ai>
2021-10-22 12:06:43 +0300

    add support for writing tests

---
+++ file-spy-test/ObserverTest.cpp
@@ -0,0 +1,26 @@
+#include "gmock/gmock.h"
+
+#include "domain/Observer.hpp"
+
+using namespace testing;
+
+TEST(ObserverTest, check_for_changes_finds_delete) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(ObserverTest, check_for_changes_finds_update) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(ObserverTest, check_for_changes_finds_modify) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
